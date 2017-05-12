1e320a010238a45389b4ac52c947caf3fd57ede6
MIKE NGUYEN <vmn@10-150-44-13.dhcp-m.deakin.edu.au>
2017-05-12 18:21:34 +1000

    commit some view controller

--- Music Player/Music Player/MyMusicViewController.swift
+++ Music Player/Music Player/MyMusicViewController.swift
@@ -95,3 +95,123 @@ class MyMusicViewController: UIViewController {
     }
 }
 
+/*
+ import UIKit
+ import AVFoundation
+ import MediaPlayer
+ 
+ class MyMusicViewController: UIViewController {
+ 
+ 
+ var timer: Timer?
+ @IBOutlet weak var label: UILabel!
+ @IBOutlet weak var myImageView: UIImageView!
+ 
+ 
+ //Play Music
+ @IBAction func play(_ sender: Any) {
+ if audioStuffed == true && audioPlayer.isPlaying == false
+ {
+ audioPlayer.play()
+ }
+ }
+ 
+ @IBAction func pause(_ sender: Any)
+ {
+ if audioStuffed == true && audioPlayer.isPlaying
+ {
+ audioPlayer.pause()
+ }
+ }
+ 
+ 
+ //Previous Music
+ @IBAction func prev(_ sender: Any) {
+ 
+ if thisSong != 0 && audioStuffed == true
+ {
+ playThis(thisOne: songs[thisSong-1])
+ thisSong -= 1
+ label.text = songs[thisSong]
+ }
+ else
+ {
+ 
+ }
+ }
+ 
+ @IBAction func next(_ sender: Any) {
+ 
+ if thisSong < songs.count-1 && audioStuffed == true
+ {
+ playThis(thisOne: songs[thisSong+1])
+ thisSong += 1
+ label.text = songs[thisSong]
+ }
+ else
+ {
+ 
+ }
+ }
+ 
+ 
+ //Slider Timer Music
+ @IBOutlet weak var sliderOutlet: UISlider!
+ 
+ @IBAction func sliderChangeValue(_ sender: Any) {
+ }
+ 
+ 
+ //slider volume
+ 
+ @IBAction func slider(_ sender: UISlider)
+ {
+ if audioStuffed == true
+ {
+ audioPlayer.volume = sender.value
+ }
+ }
+ 
+ func playThis(thisOne:String)
+ {
+ sliderOutlet.minimumValue = 0
+ 
+ do
+ {
+ let audioPath = Bundle.main.path(forResource: thisOne, ofType: ".mp3")
+ try audioPlayer = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: audioPath!) as URL)
+ sliderOutlet.maximumValue = Float(audioPlayer.duration)
+ timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(changeSliderValueFollowPlayerCurTime), userInfo: nil, repeats: true)
+ audioPlayer.play()
+ }
+ catch
+ {
+ print ("ERROR")
+ }
+ }
+ 
+ 
+ func changeSliderValueFollowPlayerCurTime(){
+ let curValue = Float(audioPlayer.currentTime)
+ sliderOutlet.value = curValue
+ }
+ override func viewDidLoad() {
+ 
+ super.viewDidLoad()
+ //label.text = songs[thisSong]
+ 
+ 
+ //sliderOutlet.minimumValue = 0
+ 
+ 
+ }
+ 
+ override func didReceiveMemoryWarning() {
+ super.didReceiveMemoryWarning()
+ // Dispose of any resources that can be recreated.
+ }
+ }
+ 
+
+ */
+
--- Music Player/Music Player/ViewController.swift
+++ Music Player/Music Player/ViewController.swift
@@ -8,25 +8,53 @@
 
 import UIKit
 
-
 class ViewController: UIViewController {
     
+    var gravity : UIGravityBehavior?
+    var animator: UIDynamicAnimator?
+    var gameTimer: Timer?
+    
+    func addBalloons( _ : Any)
+    {
+        
+        let xCoordinate = arc4random() % UInt32(self.view.bounds.width)
+        
+        let btn = UIButton(frame: CGRect(x: Int(xCoordinate), y: 60, width: 50, height: 50))
+        btn.setImage(UIImage(named: "multiple"), for: .normal)
+        btn.addTarget(self, action: #selector(self.didPopBalloons(sender:)), for: .touchUpInside)
+        self.view.addSubview(btn)
+        
+        gravity?.addItem(btn as UIView)
+    }
     
     
+    func didPopBalloons(sender : UIButton)
+    {
+        sender.setImage(UIImage(named: "music"), for: .normal)
+        UIView.animate(withDuration: 0.3, animations: {sender.alpha = 0},
+                       completion: { (true) in sender.removeFromSuperview()})
+    }
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //get timer to show balloons
+        gameTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector (ViewController.addBalloons(_ :)), userInfo: nil, repeats: true)
+        
+        animator = UIDynamicAnimator(referenceView: self.view)
+        gravity = UIGravityBehavior( items: [])
+        
+        let vector = CGVector(dx: 0.0, dy: 0.1)
+        gravity?.gravityDirection = vector
+        animator?.addBehavior(gravity!)
+        
         // Do any additional setup after loading the view, typically from a nib.
-        
-        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
