ebb39d8805cf258c9aa41d52ff5feb274ed5a07f
MIKE NGUYEN <vmn@10-144-61-188.dhcp-m.deakin.edu.au>
2017-05-18 16:19:53 +1000

    commit viewcontroller app

--- Music Player/Music Player/MyMusicViewController.swift
+++ Music Player/Music Player/MyMusicViewController.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 import AVFoundation
+import MediaPlayer
 
 class MyMusicViewController: UIViewController {
     
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var myImageView: UIImageView!
+
+    
+    var timer: Timer?
+
     
     
-    //Play Music
+    //Play music
     @IBAction func play(_ sender: Any) {
       if audioStuffed == true && audioPlayer.isPlaying == false
         {
@@ -23,8 +28,9 @@ class MyMusicViewController: UIViewController {
         }
     }
     
-    @IBAction func pause(_ sender: Any)
-    {
+    
+    //Pause music
+    @IBAction func pause(_ sender: Any) {
         if audioStuffed == true && audioPlayer.isPlaying
         {
             audioPlayer.pause()
@@ -32,7 +38,7 @@ class MyMusicViewController: UIViewController {
     }
     
     
-    //Previous Music
+    //Previous song
     @IBAction func prev(_ sender: Any) {
   
         if thisSong != 0 && audioStuffed == true
@@ -47,6 +53,7 @@ class MyMusicViewController: UIViewController {
         }
     }
     
+    //Next song
     @IBAction func next(_ sender: Any) {
  
         if thisSong < songs.count-1 && audioStuffed == true
@@ -61,27 +68,108 @@ class MyMusicViewController: UIViewController {
         }
     }
     
+    
+    //Slider Timer Music
+    @IBOutlet weak var sliderOutlet: UISlider!
+    
+    
+    @IBAction func sliderChangeValue(_ sender: Any) {
+        audioPlayer.pause()
+        let curTime = sliderOutlet.value
+        audioPlayer.currentTime = TimeInterval(curTime)
+        audioPlayer.play()
+    }
+    
+    // Option volume of song
+    /*
     @IBAction func slider(_ sender: UISlider)
     {
         if audioStuffed == true
         {
             audioPlayer.volume = sender.value
         }
-    }
+    }*/
     
+    
+    
+    //Play music with time of Song
     func playThis(thisOne:String)
     {
+        sliderOutlet.minimumValue = 0
+        
+        //play music on background
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback, with: .defaultToSpeaker)
+            
+        }catch{
+        }
+        
+        //Play Music
+        
         do
         {
             let audioPath = Bundle.main.path(forResource: thisOne, ofType: ".mp3")
             try audioPlayer = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: audioPath!) as URL)
+            sliderOutlet.maximumValue = Float(audioPlayer.duration)
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(changeSliderValueFollowPlayerCurTime), userInfo: nil, repeats: true)
+            
+            //catch event of music
+            //audioPlayer.delegate = self
+            
             audioPlayer.play()
+            
+            //View info at Background
+            
+            //View information of Song such as: song name, song singer
+            let artwork = MPMediaItemArtwork(image: #imageLiteral(resourceName: "passenger"))
+            
+            MPNowPlayingInfoCenter.default().nowPlayingInfo = [
+                MPMediaItemPropertyTitle: "Alone",
+                MPMediaItemPropertyArtist: "Unknow",
+                MPMediaItemPropertyPlaybackDuration:
+                    audioPlayer.duration,
+                MPMediaItemPropertyArtwork: artwork
+                
+            ]
+            UIApplication.shared.beginReceivingRemoteControlEvents()
+            becomeFirstResponder()
+            
         }
         catch
         {
             print ("ERROR")
         }
     }
+
+    
+    //Change value of song
+    func changeSliderValueFollowPlayerCurTime(){
+        let curValue = Float(audioPlayer.currentTime)
+        sliderOutlet.value = curValue
+    }
+    
+    
+    override func remoteControlReceived(with event: UIEvent?) {
+        if let event = event {
+            if event.type == .remoteControl{
+                switch event.subtype {
+                case .remoteControlPlay:
+                    audioPlayer.play()
+                case .remoteControlPause:
+                    audioPlayer.pause()
+                case .remoteControlNextTrack:
+                    print("Next Song")
+                case .remoteControlPreviousTrack:
+                    print("Previous Song")
+                    
+                default:
+                    print("Not configure!")
+                }
+            }
+        }
+    }
+
     
     override func viewDidLoad() {
         
@@ -95,123 +183,35 @@ class MyMusicViewController: UIViewController {
     }
 }
 
-/*
- import UIKit
- import AVFoundation
- import MediaPlayer
- 
- class MyMusicViewController: UIViewController {
- 
- 
- var timer: Timer?
- @IBOutlet weak var label: UILabel!
- @IBOutlet weak var myImageView: UIImageView!
- 
- 
- //Play Music
- @IBAction func play(_ sender: Any) {
- if audioStuffed == true && audioPlayer.isPlaying == false
- {
- audioPlayer.play()
- }
- }
- 
- @IBAction func pause(_ sender: Any)
- {
- if audioStuffed == true && audioPlayer.isPlaying
- {
- audioPlayer.pause()
- }
- }
- 
- 
- //Previous Music
- @IBAction func prev(_ sender: Any) {
- 
- if thisSong != 0 && audioStuffed == true
- {
- playThis(thisOne: songs[thisSong-1])
- thisSong -= 1
- label.text = songs[thisSong]
- }
- else
- {
- 
- }
- }
- 
- @IBAction func next(_ sender: Any) {
- 
- if thisSong < songs.count-1 && audioStuffed == true
- {
- playThis(thisOne: songs[thisSong+1])
- thisSong += 1
- label.text = songs[thisSong]
- }
- else
- {
- 
- }
- }
- 
- 
- //Slider Timer Music
- @IBOutlet weak var sliderOutlet: UISlider!
- 
- @IBAction func sliderChangeValue(_ sender: Any) {
- }
- 
- 
- //slider volume
- 
- @IBAction func slider(_ sender: UISlider)
- {
- if audioStuffed == true
- {
- audioPlayer.volume = sender.value
- }
- }
- 
- func playThis(thisOne:String)
- {
- sliderOutlet.minimumValue = 0
- 
- do
- {
- let audioPath = Bundle.main.path(forResource: thisOne, ofType: ".mp3")
- try audioPlayer = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: audioPath!) as URL)
- sliderOutlet.maximumValue = Float(audioPlayer.duration)
- timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(changeSliderValueFollowPlayerCurTime), userInfo: nil, repeats: true)
- audioPlayer.play()
- }
- catch
- {
- print ("ERROR")
- }
- }
- 
- 
- func changeSliderValueFollowPlayerCurTime(){
- let curValue = Float(audioPlayer.currentTime)
- sliderOutlet.value = curValue
- }
- override func viewDidLoad() {
- 
- super.viewDidLoad()
- //label.text = songs[thisSong]
- 
- 
- //sliderOutlet.minimumValue = 0
- 
- 
- }
- 
- override func didReceiveMemoryWarning() {
- super.didReceiveMemoryWarning()
- // Dispose of any resources that can be recreated.
- }
- }
- 
 
- */
+//Auto play next song
+
+extension MyMusicViewController : AVAudioPlayerDelegate{
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        if flag {
+            do
+            {
+                let audioPath = Bundle.main.path(forResource: "love", ofType: ".mp3")
+                try audioPlayer = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: audioPath!) as URL)
+                sliderOutlet.maximumValue = Float(audioPlayer.duration)
+                timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(changeSliderValueFollowPlayerCurTime), userInfo: nil, repeats: true)
+                
+                //catch event of music
+                audioPlayer.delegate = self
+                
+                audioPlayer.play()
+                
+            }
+            catch
+            {
+                print ("ERROR")
+            }
+            
+        }
+    }
+}
+
+
+
+
 
