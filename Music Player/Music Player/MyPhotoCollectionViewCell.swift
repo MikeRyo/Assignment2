5152f1a5ab7e474a5020db7a867f9e7fcce7ec9a
RYO HUYNH <mqhuynh@10-150-45-171.dhcp-m.deakin.edu.au>
2017-05-18 11:59:00 +1000

    Photo EDITING

---
+++ Music Player/Music Player/MyPhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyPhotoCollectionViewCell.swift
+//  Music Player
+//
+//  Created by RYO HUYNH on 18/05/2017.
+//  Copyright © 2017 Deakin University. All rights reserved.
+//
+
+import UIKit
+
+class MyPhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myImageView: UIImageView!
+}
--- Music Player/Music Player/MyPhotoViewController.swift
+++ Music Player/Music Player/MyPhotoViewController.swift
@@ -8,12 +8,16 @@
 
 import UIKit
 
-class MyPhotoViewController: UIViewController {
+class MyPhotoViewController: UIViewController , UICollectionViewDelegate, UICollectionViewDataSource{
 
+    @IBOutlet weak var MyPhotoCollection: UICollectionView!
+    var images = ["Passenger-1", "ed_sheeran", "one_direction", "justin_bieber", "One_Republic"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        self.MyPhotoCollection.delegate = self
+        self.MyPhotoCollection.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +25,19 @@ class MyPhotoViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collection_cell", for: indexPath) as! MyPhotoCollectionViewCell
+        
+        // set images
+        cell.myImageView.image = UIImage(named: images[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Selected row is", indexPath.row)
+    }
 }
