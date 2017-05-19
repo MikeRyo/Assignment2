525893fa312928f4d493670ed5b07f22f332ef8c
RYO HUYNH <mqhuynh@10-150-44-70.dhcp-m.deakin.edu.au>
2017-05-19 19:11:59 +1000

    update share app

--- Music Player/Music Player/ContactUsViewController.swift
+++ Music Player/Music Player/ContactUsViewController.swift
@@ -7,29 +7,96 @@
 //
 
 import UIKit
+import Social
 
 class ContactUsViewController: UIViewController {
-
+    
+    @IBAction func facebook(_ sender: Any) {
+        
+        //Alert
+        let alert = UIAlertController(title: "Share", message: "Share this App for your friends!", preferredStyle: .actionSheet)
+        
+        //First action
+        let actionOne = UIAlertAction(title: "Share on Facebook", style: .default) { (action) in
+            
+            //Checking if user is connected to Facebook
+            if SLComposeViewController.isAvailable(forServiceType: SLServiceTypeFacebook)
+            {
+                let post = SLComposeViewController(forServiceType: SLServiceTypeFacebook)!
+                
+                post.setInitialText("Post on Facebook")
+                post.add(UIImage(named: "facebook.png"))
+                
+                self.present(post, animated: true, completion: nil)
+                
+            } else {self.showAlert(service: "Facebook")}
+        }
+        
+        
+        let actionThree = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        //Add action to action sheet
+        alert.addAction(actionOne)
+        alert.addAction(actionThree)
+        
+        //Present alert
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
+    @IBAction func twitter(_ sender: Any) {
+        //Alert
+        let alert = UIAlertController(title: "Share", message: "Share this App for your friends!", preferredStyle: .actionSheet)
+        
+        
+        //Second action
+        let actionTwo = UIAlertAction(title: "Share on Twitter", style: .default) { (action) in
+            
+            //Checking if user is connected to Facebook
+            if SLComposeViewController.isAvailable(forServiceType: SLServiceTypeTwitter)
+            {
+                let post = SLComposeViewController(forServiceType: SLServiceTypeTwitter)!
+                
+                post.setInitialText("Post on Twitter")
+                post.add(UIImage(named: "twitter.png"))
+                
+                self.present(post, animated: true, completion: nil)
+                
+            } else {self.showAlert(service: "Twitter")}
+        }
+        
+        let actionThree = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        //Add action to action sheet
+        alert.addAction(actionTwo)
+        alert.addAction(actionThree)
+        
+        //Present alert
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    func showAlert(service:String)
+    {
+        let alert = UIAlertController(title: "Error", message: "You are not connected to \(service)", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Dismiss", style: .cancel, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
+
